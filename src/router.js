2390c574045e64275ffa0250cc0ecd9130cabbd6
Antoine Aube <aaube3@gmail.com>
2024-01-25 14:11:23 -0700

    refactor router file to use router 4 since we are using Vue3

--- src/router.js
+++ src/router.js
@@ -1,22 +1,23 @@
-import Vue from "vue";
-import Router from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import RegistrationForm from "./components/RegistrationForm.vue";
 import LoginForm from "./components/LoginForm.vue";
 
-Vue.use(Router);
+const routes = [
+  {
+    path: "/register",
+    name: "/register",
+    component: RegistrationForm,
+  },
+  {
+    path: "/login",
+    name: "/login",
+    component: LoginForm,
+  },
+];
 
-export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/register",
-      name: "/register",
-      component: RegistrationForm,
-    },
-    {
-      path: "/login",
-      name: "/login",
-      component: LoginForm,
-    },
-  ],
+const router = createRouter({
+  history: createWebHistory(process.env.BASER_URL),
+  routes,
 });
+
+export default router;
