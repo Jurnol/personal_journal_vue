a38270daa56c7b0d2efaa3b3dfa52ef743dabb22
Antoine Aube <aaube3@gmail.com>
2024-01-25 21:23:07 -0700

    compoent: login form?

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>User Login</h1>
+    <form @submit.prevent="loginUser">
+      <input type="email" v-model="user.email" placeholder="Email" />
+      <input type="password" v-model="user.password" placeholder="Password" />
+      <p v-if="errorMessage">{{ errorMessage }}</p>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { toSnakeCase } from "@/utils";
+import axios from "axios";
+// import { useRouter } from "vue-router";
+
+export default {
+  data() {
+    return {
+      user: {
+        email: "",
+        password: "",
+      },
+      errorMessage: "",
+    };
+  },
+  methods: {
+    async loginUser() {
+      // const router = useRouter();
+      const userData = toSnakeCase(this.user);
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/api/v1/login",
+          {
+            userData,
+          }
+        );
+        // Here you might want to do something with the response, like storing a token
+        console.log(response.data); // Log or handle response data
+
+        // Redirect to another page after login, adjust the route as needed
+        // router.push({ name: "dashboard" }) similar to the registration page, will likely go to a dashboard
+      } catch (error) {
+        //Take into consideration the thing from the registration page, where there response didn't have
+        //User similar error handling as the registration component
+        this.errorMessage = error.response.data.message || "Login failed";
+      }
+    },
+  },
+};
+</script>
