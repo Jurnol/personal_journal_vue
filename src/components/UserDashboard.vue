c982832da05aa16d81e67b8e306d34cf4c82abce
Antoine Aube <aaube3@gmail.com>
2024-02-08 19:15:42 -0700

    call to get user info with sessionStorage Token success

--- src/components/UserDashboard.vue
+++ src/components/UserDashboard.vue
@@ -1,5 +1,52 @@
 <template>
-  <div></div>
+  <div>
+    <h1>User Dashboard</h1>
+    <div v-if="userInfo">
+      <p>Welcome, {{ userInfo.user_info.first_name }}</p>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+// import { toSnakeCase } from "@/utils";
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+export default {
+  setup() {
+    const userInfo = ref(null);
+
+    const fetchUserInfo = async () => {
+      const token = sessionStorage.getItem("session-token");
+      if (!token) {
+        console.error("No token found for this session");
+        return;
+      }
+
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/v1/journals",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        userInfo.value = response.data;
+        console.log(response);
+        console.log(userInfo);
+      } catch (error) {
+        console.log("Error when retrieving user_info");
+      }
+    };
+
+    onMounted(() => {
+      fetchUserInfo();
+    });
+
+    return {
+      userInfo,
+    };
+  },
+};
+</script>
